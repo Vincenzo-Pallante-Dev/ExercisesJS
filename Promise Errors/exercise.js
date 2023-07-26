9b2dc3d2a598b292192b64e592f91e6c4ae795e0
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-07-26 20:15:22 +0200

    feat: exercise js

---
+++ Promise Errors/exercise.js
@@ -0,0 +1,25 @@
+const isLogged = true;
+
+let rand = Math.random();
+
+let controllo = new Promise((resolve, reject) => {
+  if (isLogged == true) {
+    resolve(rand);
+  } else {
+    reject(new Error("Error!"));
+  }
+});
+
+let controllo2 = new Promise((resolve, reject) => {
+  if (rand > 0.5) {
+    resolve(console.log(`{ nome: "jhon", età: 24 }`));
+  } else {
+    reject(new Error("Error!"));
+  }
+});
+
+controllo
+  .then(controllo2)
+  .then((y) => console.log(y))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("finally!"));
